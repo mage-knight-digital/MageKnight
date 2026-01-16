330c5fc0e92350d82338f9c6738ee8dec351dba9
Erick Shaffer <erick.shaffer321@gmail.com>
2026-01-16 13:26:54 -0700

    chore: Remove unused Combat components
    Delete dead code:
    - PhaseRail (replaced by VerticalPhaseRail)
    - PhaseIndicator, CombatSummary, EnemyList, CombatActions (never imported)
    - SiteArtwork, CombatInfoPanel (created but unused)

--- packages/client/src/components/Combat/index.ts
+++ packages/client/src/components/Combat/index.ts
@@ -3,9 +3,5 @@
  */
 
 export { CombatOverlay } from "./CombatOverlay";
-export { PhaseRail } from "./PhaseRail";
 export { VerticalPhaseRail } from "./VerticalPhaseRail";
-export { PhaseIndicator } from "./PhaseIndicator";
 export { EnemyCard } from "./EnemyCard";
-export { EnemyList } from "./EnemyList";
-export { CombatSummary } from "./CombatSummary";
