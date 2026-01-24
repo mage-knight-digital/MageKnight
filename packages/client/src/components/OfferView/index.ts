45d9ce9a48d6baa039408185e943fcaef320b0cc
Erick Shaffer <erick.shaffer321@gmail.com>
2026-01-23 20:47:51 -0700

    chore(client): Remove dead offer pane components
    The OfferView now uses PixiOfferView exclusively for rendering. The
    old HTML/React pane components were never used and caused confusion
    when trying to modify the offer display.
    
    Removed dead code:
    - AAOfferPane.tsx
    - SpellOfferPane.tsx
    - UnitOfferPane.tsx
    - PixiOfferCards.tsx
    - OfferCard.tsx
    - OfferTray.tsx
    - OfferView.css
    
    Updated index.ts to only export the active OfferView component.

--- packages/client/src/components/OfferView/index.ts
+++ packages/client/src/components/OfferView/index.ts
@@ -1,6 +1,1 @@
 export { OfferView, type OfferViewProps, type OfferPane } from "./OfferView";
-export { OfferTray, type OfferTrayProps } from "./OfferTray";
-export { OfferCard, type OfferCardProps, type OfferCardType } from "./OfferCard";
-export { UnitOfferPane } from "./UnitOfferPane";
-export { SpellOfferPane } from "./SpellOfferPane";
-export { AAOfferPane } from "./AAOfferPane";
