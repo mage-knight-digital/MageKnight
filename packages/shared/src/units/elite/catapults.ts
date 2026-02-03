cd7927c520a2a289fb902547800ff6eda1091fea
Erick Shaffer <erick.shaffer@meraki.net>
2026-02-03 15:30:04 -0700

    fix: correct Catapults siege abilities (#281)

--- packages/shared/src/units/elite/catapults.ts
+++ packages/shared/src/units/elite/catapults.ts
@@ -2,7 +2,7 @@
  * Catapults unit definition
  */
 
-import { ELEMENT_PHYSICAL } from "../../elements.js";
+import { ELEMENT_FIRE, ELEMENT_ICE, ELEMENT_PHYSICAL } from "../../elements.js";
 import type { UnitDefinition } from "../types.js";
 import {
   UNIT_TYPE_ELITE,
@@ -11,6 +11,7 @@ import {
   UNIT_ABILITY_SIEGE_ATTACK,
 } from "../constants.js";
 import { UNIT_CATAPULTS } from "../ids.js";
+import { MANA_BLUE, MANA_RED } from "../../ids.js";
 
 export const CATAPULTS: UnitDefinition = {
   id: UNIT_CATAPULTS,
@@ -22,7 +23,9 @@ export const CATAPULTS: UnitDefinition = {
   resistances: [],
   recruitSites: [RECRUIT_SITE_KEEP, RECRUIT_SITE_CITY],
   abilities: [
-    { type: UNIT_ABILITY_SIEGE_ATTACK, value: 4, element: ELEMENT_PHYSICAL },
+    { type: UNIT_ABILITY_SIEGE_ATTACK, value: 3, element: ELEMENT_PHYSICAL },
+    { type: UNIT_ABILITY_SIEGE_ATTACK, value: 5, element: ELEMENT_FIRE, manaCost: MANA_RED },
+    { type: UNIT_ABILITY_SIEGE_ATTACK, value: 5, element: ELEMENT_ICE, manaCost: MANA_BLUE },
   ],
   copies: 3,
 };
