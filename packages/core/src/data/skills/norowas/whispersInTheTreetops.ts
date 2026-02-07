1ef4e13aa4e96bf596252488835299b773e0020a
Erick Shaffer <erick.shaffer321@gmail.com>
2026-02-07 08:35:00 -0700

    feat: implement Whispers in the Treetops skill (#325)

--- packages/core/src/data/skills/norowas/whispersInTheTreetops.ts
+++ packages/core/src/data/skills/norowas/whispersInTheTreetops.ts
@@ -1,10 +1,16 @@
 /**
  * Whispers in the Treetops - Norowas Skill
  * @module data/skills/norowas/whispersInTheTreetops
+ *
+ * Once a round: Flip this to gain one white crystal to your inventory
+ * and one green mana token.
  */
 
 import type { SkillId } from "@mage-knight/shared";
+import { MANA_GREEN, MANA_WHITE } from "@mage-knight/shared";
 import { CATEGORY_SPECIAL } from "../../../types/cards.js";
+import { EFFECT_GAIN_CRYSTAL, EFFECT_GAIN_MANA } from "../../../types/effectTypes.js";
+import { compound } from "../../effectHelpers.js";
 import { type SkillDefinition, SKILL_USAGE_ONCE_PER_ROUND } from "../types.js";
 
 export const SKILL_NOROWAS_WHISPERS_IN_THE_TREETOPS = "norowas_whispers_in_the_treetops" as SkillId;
@@ -16,4 +22,10 @@ export const whispersInTheTreetops: SkillDefinition = {
   description: "Flip to gain 1 white crystal and 1 green mana token",
   usageType: SKILL_USAGE_ONCE_PER_ROUND,
   categories: [CATEGORY_SPECIAL],
+  effect: compound([
+    // Gain one white crystal (permanent, to inventory)
+    { type: EFFECT_GAIN_CRYSTAL, color: MANA_WHITE },
+    // Gain one green mana token (temporary)
+    { type: EFFECT_GAIN_MANA, color: MANA_GREEN },
+  ]),
 };
