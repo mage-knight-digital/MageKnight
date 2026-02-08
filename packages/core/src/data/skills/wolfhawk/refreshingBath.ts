c9a405c4394bdbd194330154d581b816d4c7161c
eshaffer321 <erick.shaffer321@gmail.com>
2026-02-08 06:10:26 -0700

    feat: implement Wolfhawk Refreshing Bath skill (#350) (#894)

--- packages/core/src/data/skills/wolfhawk/refreshingBath.ts
+++ packages/core/src/data/skills/wolfhawk/refreshingBath.ts
@@ -4,16 +4,23 @@
  */
 
 import type { SkillId } from "@mage-knight/shared";
+import { MANA_BLUE } from "@mage-knight/shared";
 import { CATEGORY_HEALING, CATEGORY_SPECIAL } from "../../../types/cards.js";
+import { EFFECT_GAIN_CRYSTAL, EFFECT_GAIN_HEALING } from "../../../types/effectTypes.js";
+import { compound } from "../../effectHelpers.js";
 import { type SkillDefinition, SKILL_USAGE_ONCE_PER_ROUND } from "../types.js";
 
 export const SKILL_WOLFHAWK_REFRESHING_BATH = "wolfhawk_refreshing_bath" as SkillId;
 
 export const refreshingBath: SkillDefinition = {
   id: SKILL_WOLFHAWK_REFRESHING_BATH,
-    name: "Refreshing Bath",
-    heroId: "wolfhawk",
-    description: "Flip for Heal 1 and 1 blue crystal (except combat)",
-    usageType: SKILL_USAGE_ONCE_PER_ROUND,
-    categories: [CATEGORY_HEALING, CATEGORY_SPECIAL],
+  name: "Refreshing Bath",
+  heroId: "wolfhawk",
+  description: "Flip for Heal 1 and 1 blue crystal (except combat)",
+  usageType: SKILL_USAGE_ONCE_PER_ROUND,
+  categories: [CATEGORY_HEALING, CATEGORY_SPECIAL],
+  effect: compound([
+    { type: EFFECT_GAIN_HEALING, amount: 1 },
+    { type: EFFECT_GAIN_CRYSTAL, color: MANA_BLUE },
+  ]),
 };
